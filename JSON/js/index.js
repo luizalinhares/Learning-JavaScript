2f7a12b643258f271f60849f434230a10e4f5abb
luizalinhares <100442678+luizalinhares@users.noreply.github.com>
2022-06-29 23:38:41 -0300

    Update index.js

--- JSON/js/index.js
+++ JSON/js/index.js
@@ -1,11 +1,3 @@
-/*JSON - Javascript object Notification
-
-é basicamente uma forma de converter um objeto em texto ou um texto em um objeto
-
-é usado principalmente para transmitir dados entre sistemas de forma simples
-
-utiliza-se dois métodos para trabalhar com JSON:
-
 JSON.parse() -> converte texto no padrão JSON em objetos
 JSON.stringify() -> Converte objetos em texto padrão JSON
 
@@ -22,7 +14,7 @@ JSON.stringify() -> Converte objetos em texto padrão JSON
 let texto = JSON.stringify(carro);
 
 
-//colocou o texto no nosso html
+//colocou o texto no html
 document.getElementById('area').innerHTML = texto;
 
 //converteu o texto em objeto
@@ -34,12 +26,3 @@ console.log(obj.ano);
 console.log(obj.marca);
 console.log(obj.cor[3])*/
 
-const ajax = new XMLHttpRequest();
-ajax.open( 'GET','http://viacep.com.br/ws/31150190/json/' );
-ajax.send();
-
-ajax.onload = function () {
-  document.getElementById('area').innerHTML = this.responseText;
-  let obj = JSON.parse(this.responseText);
-  alert(obj.ddd);
-}
